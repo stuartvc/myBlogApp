e3403c8e7ed27b369d616a9b93a8e0d2a17145cf
Stuart Van Campen <stuart.vancampen@gmail.com>
2015-11-17 01:32:01 -0800

    simplified user objects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users, each_serializer: UserPreviewSerializer
+    render json: @users, each_serializer: UserSerializer
   end
 
   # GET /users/1
