d4e03cc7e92fd73069b060a4f041ea979b4c5cc2
Stuart Van Campen <stuart.vancampen@gmail.com>
2015-11-23 23:35:42 -0800

    aws cognito

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,10 +21,10 @@ class ApplicationController < ActionController::API
 	end
 
 	def awsVerification
-		s3Client = Aws::S3::Client.new
+		s3Client = Aws::CognitoIdentity::Client.new
 		resp = s3Client.get_open_id_token_for_developer_identity({
 			identity_pool_id: "us-east-1:31d8dd8e-2283-4476-9eae-3dfdc271af50", 
-			identity_id: @current_user.id,
+			identity_id: @current_user.id.to_s,
 			logins: { # required
     			"IdentityProviderName" => "login.stuartvancampen.myblog"
   			}
